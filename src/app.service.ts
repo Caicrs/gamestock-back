41c635d846a86b811819d6136e6610e645425621
Caic Rocha <caicrochask8@gmail.com>
2022-10-02 10:57:24 -0400

    Change link for swagger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! 🚀\n Please check http://localhost:3333/api for Swagger docs...';
+    return 'Server is running! 🚀\n Please check https://gamestock-api.herokuapp.com/api for Swagger docs...';
   }
 }
